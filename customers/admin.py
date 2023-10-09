39a5a64195e0cb90584a94ed67bd7c2bcf150d76
verafadeeva <lunatik.yar@gmail.com>
2023-10-09 10:18:21 +0300

    Fix some code style

--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from customers.models import Customer
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email')
+    empty_value_display = '-пусто-'
--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from orders.models import Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'robot_serial', 'is_done')
+    empty_value_display = '-пусто-'
